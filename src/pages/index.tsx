b303a0b45d9c2800c51d6894a624870bf3dac94f
kniest94 <71277111+kniest94@users.noreply.github.com>
2022-03-12 10:45:27 +0100

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -43,7 +43,7 @@ export default function Home(): JSX.Element {
       <main>
         <HomepageFeatures />
         
-        {<SimpleMap />}
+        {/* <SimpleMap /> */}
       </main>
 
     </Layout>
