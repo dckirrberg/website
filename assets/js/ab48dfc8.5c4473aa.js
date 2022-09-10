1509c31df9ac10f7c26fc13f94aac9b5f81a7562
kniest94 <kniest94@users.noreply.github.com>
2022-09-10 14:37:00 +0000

    deploy: 957513f734dd8d9db1c17701d476bc61d2925f0a

---
+++ assets/js/ab48dfc8.5c4473aa.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdckirrberg=self.webpackChunkdckirrberg||[]).push([[5819],{3673:function(l){l.exports=JSON.parse('{"allTagsPath":"/blog/tags","slug":"/blog/tags/spielplan","name":"Spielplan","count":1,"permalink":"/blog/tags/spielplan"}')}}]);
