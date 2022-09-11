1cc4b1eb598c5fb6f632144c9577a2ce452e6631
kniest94 <71277111+kniest94@users.noreply.github.com>
2022-09-11 11:59:25 +0200

    Update Ergebnisse.tsx

--- src/pages/Ergebnisse.tsx
+++ src/pages/Ergebnisse.tsx
@@ -10,26 +10,22 @@ export default function Teams() {
       <div className="hero">
         <div className="container">
          <div className="text--center margin-bottom--lg">
-            <h2 className="hero__title margin--none">Unsere Mannschaften & Ergebnisse</h2>  
-            <br></br> 
-            <h3>Unsere Teams</h3>
-            Wir freuen uns mit direkt 2 Teams in die neue Saision starten zu dürfen.
-            <br></br>
-            Die Mannschaften inkl. Fotos, werden in den kommenden Wochen hier veröffentlicht.
-
-            <br></br>
-            Der Spielplan steht zudem im Bereich "Downloads" zum  <a href="/dateien/Spielplan 22_23.jpg" target="_blank">Download</a> bereit:
-            <br></br><br></br>           
-          </div> 
+            <h2 className="hero__title margin--none">Unsere Mannschaften & Ergebnisse</h2>             
+         </div> 
           <br></br>
           <div className="text--center margin-bottom--lg">
             <br></br> 
-            <h3>Spielplan | Tabelle | Spieler</h3>
+            <h3>BULLs Ligaübersicht</h3>
+            <a href="https://www.sadv.2k-dart-software.de/index.php/de/component/dartliga/index.php?option=com_dartliga&controller=showligagameplan&layout=showdashboard&filVbKey=100&filCompKey=1&filSaiKey=119&filVbsubKey=31&filStaffKey=793&filStaffFsGrpdataKey=0#" target="_blank">
+            Direktlink zum Spielplan
+            </a>
             <div className={clsx('hero', styles.heroBanner)}>
              <iframe width="1280" height="820" src="https://www.sadv.2k-dart-software.de/index.php/de/component/dartliga/index.php?option=com_dartliga&controller=showligagameplan&layout=showdashboard&filVbKey=100&filCompKey=1&filSaiKey=119&filVbsubKey=31&filStaffKey=793&filStaffFsGrpdataKey=0#"></iframe>
-  
             </div>
          </div>   
+         
+         <br></br>
+            Der Spielplan steht zudem zum <a href="/dateien/Spielplan 22_23.jpg" target="_blank">Download</a> bereit:
         </div>
       </div>
     </Layout>
