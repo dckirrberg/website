9ff19f3bf9562f70343307a96414b6f693464097
kniest94 <71277111+kniest94@users.noreply.github.com>
2024-08-30 14:52:23 +0200

    upload Saisonplan

--- src/pages/Downloads.tsx
+++ src/pages/Downloads.tsx
@@ -20,7 +20,7 @@ export default function Team() {
                 <div className="col"><a href="/dateien/Beitragsordnung.pdf" target="_blank">Beitragsordnung.pdf</a></div>
                 <div className="col"><a href="/dateien/GO Mitgliederversammlung.pdf" target="_blank">Geschäftsordnung der Mitgliederversammlung.pdf</a></div>
                 <div className="col"><a href="/dateien/Wahlordnung.pdf" target="_blank">Wahlordnung.pdf</a></div>
-                <div className="col"> <a href="/dateien/Saisonplan_DCK_2024_2024.pdf" target="_blank">Saisonplan_DCK_2024_2024.pdf</a></div>
+                <div className="col"> <a href="/dateien/Saisonplan_DCK_2024_2025.pdf" target="_blank">Saisonplan_DCK_2024_2025.pdf</a></div>
                 <div className="col"><a href="/dateien/Sponsorenkonzept_DCK.pdf" target="_blank">Sponsorenkonzept_DCK.pdf</a></div>
               </div>
             </div> 
