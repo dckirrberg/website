5b294c2a834e0e0a6daaf5f629cee79a84902c83
kniest94 <kniest94@gmail.com>
2025-08-02 11:03:31 +0200

    update Spielplaene

--- src/pages/Downloads.tsx
+++ src/pages/Downloads.tsx
@@ -20,15 +20,7 @@ export default function Team() {
                 <div className="col"><a href="/dateien/Beitragsordnung.pdf" target="_blank">Beitragsordnung.pdf</a></div>
                 <div className="col"><a href="/dateien/GO Mitgliederversammlung.pdf" target="_blank">Geschäftsordnung der Mitgliederversammlung.pdf</a></div>
                 <div className="col"><a href="/dateien/Wahlordnung.pdf" target="_blank">Wahlordnung.pdf</a></div>
-                <div className="col"><a href="/dateien/Sponsorenkonzept_DCK.pdf" target="_blank">Sponsorenkonzept_DCK.pdf</a></div>
-
-                <div className="col"> <a href="/dateien/Saisonplan_Alle_2024_2025.pdf" target="_blank">Saisonplan_Alle_2024_2025.pdf</a></div>
-                <div className="col"> <a href="/dateien/Saisonplan_DCK1_2024_2025.pdf" target="_blank">Saisonplan_DCK1_2024_2025.pdf</a></div>
-                <div className="col"> <a href="/dateien/Saisonplan_DCK2_2024_2025.pdf" target="_blank">Saisonplan_DCK2_2024_2025.pdf</a></div>
-                <div className="col"> <a href="/dateien/Saisonplan_DCK3_2024_2025.pdf" target="_blank">Saisonplan_DCK3_2024_2025.pdf</a></div>
-                <div className="col"> <a href="/dateien/Saisonplan_DCK4_2024_2025.pdf" target="_blank">Saisonplan_DCK4_2024_2025.pdf</a></div>
-                <div className="col"> <a href="/dateien/Saisonplan_DCKDamen_2024_2025.pdf" target="_blank">Saisonplan_DCKDamen_2024_2025.pdf</a></div>
-                
+                <div className="col"><a href="/dateien/Sponsorenkonzept_DCK.pdf" target="_blank">Sponsorenkonzept_DCK.pdf</a></div>                
               </div>
             </div> 
 
@@ -39,12 +31,6 @@ export default function Team() {
                 <div className="col"><b>Kalenderimport</b></div>
               </div>
               <div className="column column--no-gutters">
-              <div className="col"> <a href="/dateien/icalAlle.ics" target="_blank">Spieltage_Alle.ics</a></div>
-                <div className="col"> <a href="/dateien/icalDCK1.ics" target="_blank">Spieltage_DCK1.ics</a></div>
-                <div className="col"> <a href="/dateien/icalDCK2.ics" target="_blank">Spieltage_DCK2.ics</a></div>
-                <div className="col"> <a href="/dateien/icalDCK3.ics" target="_blank">Spieltage_DCK3.ics</a></div>
-                <div className="col"> <a href="/dateien/icalDCK4.ics" target="_blank">Spieltage_DCK4.ics</a></div>
-                <div className="col"> <a href="/dateien/icalDCKDamen.ics" target="_blank">Spieltage_DCKDamen.ics</a></div>    
               </div>
             </div> 
           </div>
